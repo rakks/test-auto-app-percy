e614e8af100f0738d988f66556771daa82f7c4b1
rakks <rakkhurana@gmail.com>
2023-08-31 15:49:13 +0800

    Initial Commit

---
+++ src/main/java/io/percy/examplepercyappiumjava/Android.java
@@ -0,0 +1,74 @@
+package io.percy.examplepercyappiumjava;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.MobileBy;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import io.percy.appium.AppPercy;
+
+public class Android {
+    private static AppPercy percy;
+
+    // Hub Url to connect to Automation session
+    private static String HUB_URL = "https://hub.browserstack.com/wd/hub";
+
+    public static void main(String[] args) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        // Browserstack specific capabiilities
+        capabilities.setCapability("browserstack.user", "<USER>");
+        capabilities.setCapability("browserstack.key", "<USER_AUTH_KEY>");
+        capabilities.setCapability("browserstack.appium_version", "1.20.2");
+
+        // Percy Options
+        capabilities.setCapability("percy.enabled", "true");
+        capabilities.setCapability("percy.ignoreErrors", "true");
+
+        // App url we get post uploading in response
+        capabilities.setCapability("app", "<APP_URL>");
+        capabilities.setCapability("device", "Google Pixel 3");
+        capabilities.setCapability("os_version", "9.0");
+        capabilities.setCapability("project", "First Java Project");
+
+        // Create sessioin
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL(HUB_URL), capabilities);
+
+        // Initialize AppPercy
+        percy = new AppPercy(driver);
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // Take First Screenshot
+        percy.screenshot("First Screenshot");
+
+
+        AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Search Wikipedia")));
+        searchElement.click();
+
+        AndroidElement textInput = (AndroidElement) new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
+        textInput.sendKeys("Browserstack\n");
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // Take Second Screenshot post scrolling
+        percy.screenshot("Second Screenshot");
+
+        driver.quit();
+    }
+}
